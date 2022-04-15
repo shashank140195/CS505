621f6c8ea9ae452afa6f273f726c8b0e0891c1af
ankan <ankan.bhattacharyya.94@gmail.com>
2022-04-14 20:53:54 -0400

    Commit CEP output

--- src/main/java/cs505pubsubcep/CEP/OutputSubscriber.java
+++ src/main/java/cs505pubsubcep/CEP/OutputSubscriber.java
@@ -10,8 +10,70 @@ import javax.xml.bind.Unmarshaller;
 import java.io.StringReader;
 import java.sql.Struct;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
+
+class Alert {
+    private String zip;
+    private int count;
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Alert(String zip, int count) {
+        this.zip = zip;
+        this.count = count;
+    }
+}
+
+class RabbitEvent{
+
+    private Map<String, Alert> rabbit;
+
+    public RabbitEvent(Map<String, Alert> rabbit) {
+        this.rabbit = rabbit;
+    }
+
+    public Map<String, Alert> getRabbit() {
+        return rabbit;
+    }
+
+    public void setRabbit(Map<String, Alert> rabbit) {
+        this.rabbit = rabbit;
+    }
+}
+
+class TmpClass{
+    private Map<String, List<RabbitEvent>> TmpMap;
+
+    public Map<String, List<RabbitEvent>> getTmpMap() {
+        return TmpMap;
+    }
+
+    public void setTmpMap(Map<String, List<RabbitEvent>> tmpMap) {
+        TmpMap = tmpMap;
+    }
+
+    public TmpClass(Map<String, List<RabbitEvent>> tmpMap) {
+        TmpMap = tmpMap;
+    }
+}
+
 
 public class OutputSubscriber implements InMemoryBroker.Subscriber {
 
@@ -25,13 +87,48 @@ public class OutputSubscriber implements InMemoryBroker.Subscriber {
     public void onMessage(Object msg) {
 
         try {
-            System.out.println("OUTPUT CEP EVENT: " + String.valueOf(msg));
-            System.out.println("");
-//            String[] sstr = String.valueOf(msg).split(":");
-//            String[] outval = sstr[2].split("}");
-//            for(String tmp : outval){
-//                System.out.println(tmp);
-//            }
+            String m = String.valueOf(msg);
+            System.out.println("OUTPUT CEP EVENT: " + m);
+
+//            Pattern pattern = Pattern.compile("\\{\"event\":\\{\"zip_code\":(.*?),\"count\":(.*?)\\}\\}");
+
+            Pattern pattern = Pattern.compile("\\[(.*?)\\]");
+            Matcher matcher = pattern.matcher(m);
+
+            String first = null;
+            String second = null;
+
+            if (matcher.find()) {
+                first = matcher.group(1);
+//                second = matcher.group(2);
+            }
+            System.out.printf("First: %s\nSecond: %s\n", first, second);
+
+            first = first.replaceAll("\\{\"event\":","").replaceAll("\\}\\}","\\}").
+                    replaceAll("\\},\\{","\\};\\{");
+
+            System.out.printf("First: %s\nSecond: %s\n", first, second);
+
+            String[] alerts = first.split(";");
+            Map<Integer,Integer> zipMap = new HashMap<Integer,Integer>();
+            for(String p:alerts){
+//                System.out.println("P: "+p);
+                Pattern pat = Pattern.compile("\\{\"zip_code\":\"(.*?)\",\"count\":(.*?)\\}");
+                Matcher mat = pat.matcher(p);
+                String zip1=null;
+                String count1=null;
+                if (mat.find()) {
+                    zip1 = mat.group(1);
+                    count1 = mat.group(2);
+                }
+//                System.out.println("Zip: "+zip1+", Count: "+count1);
+                if(zip1!=null && count1!=null) {
+                    zipMap.put(Integer.parseInt(zip1), Integer.parseInt(count1));
+                }
+            }
+            System.out.println(zipMap.toString());
+
+
             //Launcher.accessCount = Long.parseLong(outval[0]);
 
         } catch(Exception ex) {
