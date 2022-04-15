3c0198a5bde1eb5a04ff89696aaa6b32a60b67e0
ankan <ankan.bhattacharyya.94@gmail.com>
2022-04-15 13:36:24 -0400

    Commit mongo db

--- src/main/java/cs505pubsubcep/database/MongoEngine.java
+++ src/main/java/cs505pubsubcep/database/MongoEngine.java
@@ -1,13 +1,20 @@
 package cs505pubsubcep.database;
 
 public class MongoEngine {
+    public MongoEngine() {
 
-    public void connect(){
-        ConnectionString connectionString = new ConnectionString("mongodb+srv://cs505:<password>@cluster0.h4pa4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+
+        ConnectionString connectionString = new ConnectionString("mongodb+srv://<username>:<username>@cluster0.dqmo2.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
         MongoClientSettings settings = MongoClientSettings.builder()
                 .applyConnectionString(connectionString)
                 .build();
         MongoClient mongoClient = MongoClients.create(settings);
-        MongoDatabase database = mongoClient.getDatabase("test");
+        MongoDatabase database = mongoClient.getDatabase("CS505Doc");
+
+
+    }
+
+    public void connect(){
+
     }
 }
